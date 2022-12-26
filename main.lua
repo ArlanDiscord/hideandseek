5f6bcbc6a780f0f4f191bbe9d1e4731fd450f372
ArlanDiscord <90557945+ArlanDiscord@users.noreply.github.com>
2022-12-26 13:03:10 +0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -11,7 +11,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/ArlanDiscord/hideands
 ======================================================================================================
 ]]--
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
-local Window = Library.CreateLib("Hide And Seek EXTREME { UI SCRIPT } v.0.0.1", "RJTheme3")
+local Window = Library.CreateLib("Hide And Seek EXTREME { UI SCRIPT } v.0.0.2", "RJTheme3")
 local gameid = game.PlaceId
 if(gameid == 205224386) then
 local Tab = Window:NewTab("Action")
@@ -20,10 +20,10 @@ local Section = Tab:NewSection("Choose Actions:")
 local CreditTab = Window:NewTab("Credits")
 local SectionCredit = CreditTab:NewSection("Discord: тарелка#0722, Youtube: @kp1ners")
 local SectionCredit2 = CreditTab:NewSection("Script made from Kazakhstan :)")
-local SectionCredit3 = CreditTab:NewSection("Script Version: v.0.0.1")
+local SectionCredit3 = CreditTab:NewSection("Script Version: v.0.0.2")
 local UpdateTab = Window:NewTab("Updates")
 local SectionUpdate = UpdateTab:NewSection("Updates:")
-local SectionUpdate1 = UpdateTab:NewSection("This is new script :)")
+local SectionUpdate1 = UpdateTab:NewSection("If you hider you can hide extremely!")
 local SectionUpdate2 = UpdateTab:NewSection("Last Update: 26.12.2022")
 print("[Cheat]: Script Hide And Seek EXTREME has been enabled!")
 
@@ -51,6 +51,10 @@ Section:NewKeybind("Reset Settings [BIND]", "If you press bind you reset jump an
 	game.Players.LocalPlayer.Character.Humanoid.HipHeight = 1.35
 end)
 
+Section:NewButton("Hide", "If you click this you hide from seek", function(c)
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(3.70294929, 264.200806, -23.7522545, 0.554864585, -2.36391635e-08, 0.831940711, 2.33399913e-08, 1, 1.28478259e-08, -0.831940711, 1.22886847e-08, 0.554864585)
+end)	
+
 else
 local Error = Window:NewTab("Error")
 local SectionError = Error:NewSection("This place is not supported for this script")
@@ -58,5 +62,5 @@ local SectionError1 = Error:NewSection("You need to join to the Hide And Seek EX
 local CreditTab = Window:NewTab("Credits")
 local SectionCredit = CreditTab:NewSection("Discord: тарелка#0722, Youtube: @kp1ners")
 local SectionCredit2 = CreditTab:NewSection("Script made from Kazakhstan :)")
-local SectionCredit3 = CreditTab:NewSection("Script Version: v.0.0.1")
+local SectionCredit3 = CreditTab:NewSection("Script Version: v.0.0.2")
 end
